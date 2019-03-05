613cae6e11a942f17eb43ed51189046b5c25b77c
culigan <culigan@gmail.com>
2019-03-05 16:05:33 -0700

    First Test

--- node-js-getting-started-master/index.js
+++ node-js-getting-started-master/index.js
@@ -2,9 +2,30 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/cost', function (req, res) {
+    res.render('pages/viewcost', calculateCost(req.query.radio, req.query.weight));
+});
+
+        function calculateCost(type, weight) {
+        
+        switch (type) {
+            case "stamped":
+                break;
+            case "metered":
+                break;
+            case "flats":
+                break;
+            case "retail":
+                break;
+            default:
+                break;
+        }
+        res.render('pages/viewcost');
+    }
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
