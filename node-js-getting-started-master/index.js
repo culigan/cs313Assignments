55a8806a4c3ecddb829b7ff28ca7b4860eabd901
culigan <culigan@gmail.com>
2019-03-05 22:57:56 -0700

    radio button are named letter

--- node-js-getting-started-master/index.js
+++ node-js-getting-started-master/index.js
@@ -3,11 +3,12 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var app = express();
-app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("views","views");
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-    res.render("Home");
+    res.render("home");
 });
 app.get('/cost', determineCost);
         
