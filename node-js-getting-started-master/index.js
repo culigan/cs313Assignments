7c692adddc09b6dfcdf95310a3fa4b3101279fae
culigan <culigan@gmail.com>
2019-03-05 18:58:15 -0700

    added calculations

--- node-js-getting-started-master/index.js
+++ node-js-getting-started-master/index.js
@@ -3,29 +3,92 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname, '/views');
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('pages/index'));
-app.get('/cost', function (req, res) {
-    res.render('pages/viewcost', calculateCost(req.query.radio, req.query.weight));
-});
+app.use('/', express.static('static/Home.html'));
+app.get('/cost', determineCost);
 
-        function calculateCost(type, weight) {
+
         
-        switch (type) {
-            case "stamped":
-                break;
-            case "metered":
-                break;
-            case "flats":
-                break;
-            case "retail":
-                break;
-            default:
-                break;
-        }
-        res.render('pages/viewcost');
-    }
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+function determineCost(req, res) {
+    var type = req.query.radio;
+    var weight = req.query.weigh;
+    var result = 0;
+
+    calculateRate(type, weight);
+}
+
+function calculateRate(type, weight) {
+
+    let result = 0;
+
+    switch (type) {
+        case "stamped":
+            if (weight <= 1.0)
+                result = .55;
+            else if (weight <= 2.0)
+                result = .7;
+            else if (weight <= 3.0)
+                result = .85;
+            else 
+                result = 1.00;
+            break;
+        case "metered":
+            if (weight <= 1.0)
+                result = .5;
+            else if (weight <= 2.0)
+                result = .65;
+            else if (weight <= 3.0)
+                result = .8;
+            else
+                result = .95;
+            break;
+        case "flats":
+            if (weight <= 1.0)
+                result = 1.0;
+            else if (weight <= 2.0)
+                result = 1.15;
+            else if (weight <= 3.0)
+                result = 1.30;
+            else if (weight <= 4.0)
+                result = 1.45;
+            else if (weight <= 5.0)
+                result = 1.60;
+            else if (weight <= 6.0)
+                result = 1.75;
+            else if (weight <= 7.0)
+                result = 1.90;
+            else if (weight <= 8.0)
+                result = 2.05;
+            else if (weight <= 9.0)
+                result = 2.20;
+            else if (weight <= 10.0)
+                result = 2.35;
+            else if (weight <= 11.0)
+                result = 2.5;
+            else if (weight <= 12.0)
+                result = 2.65;
+            else
+                result = 2.80;
+            break;
+        case "retail":
+            if (weight <= 1.0)
+                result = 3.66;
+            else if (weight <= 4.0)
+                result = 3.66;
+            else if (weight <= 8.0)
+                result = 4.39;
+            else if (weight <= 6.0)
+                result = .85;
+            else if (weight <= 12.0)
+                result = 5.19;
+            else
+                result = 5.71;
+            break;
+
+    }
+    res.render('pages/viewcost', { result: result } );
+}
